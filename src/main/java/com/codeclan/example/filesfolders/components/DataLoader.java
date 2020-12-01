09d548e26290c483a34fcd6c8588227b81a5089d
AvianLearning <iaradcliffe@hotmail.com>
2020-12-01 16:55:09 +0000

    Started on DataLoader file

---
+++ src/main/java/com/codeclan/example/filesfolders/components/DataLoader.java
@@ -0,0 +1,60 @@
+package com.codeclan.example.filesfolders.components;
+
+import com.codeclan.example.filesfolders.models.File;
+import com.codeclan.example.filesfolders.models.Folder;
+import com.codeclan.example.filesfolders.models.User;
+import com.codeclan.example.filesfolders.repositories.FileRepository;
+import com.codeclan.example.filesfolders.repositories.FolderRepository;
+import com.codeclan.example.filesfolders.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    FolderRepository folderRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    FileRepository fileRepository;
+
+    public DataLoader() {
+
+    }
+
+    public void run(ApplicationArguments args) {
+        User iain = new User("Iain");
+        userRepository.save(iain);
+
+        User aileen = new User("Aileen");
+        userRepository.save(aileen);
+
+        User isaac = new User("Isaac");
+        userRepository.save(isaac);
+
+        Folder folder1 = new Folder("CodeClan Work", iain);
+        folderRepository.save(folder1);
+
+        Folder folder2 = new Folder("Photos", aileen);
+        folderRepository.save(folder2);
+
+        Folder folder3 = new Folder("Minecraft", isaac);
+        folderRepository.save(folder3);
+
+        File file1 = new File("Encapsulation", ".md", 50, folder1);
+        fileRepository.save(file1);
+
+        File file2 = new File("HolidayLandscape", ".jpg", 40, folder2);
+        fileRepository.save(file2);
+
+        File file3 = new File("Zombies", ".txt", 100, folder3);
+        fileRepository.save(file3);
+
+
+    }
+}
