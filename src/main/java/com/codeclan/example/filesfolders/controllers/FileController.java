1ad57ebcdd5059479e4b51c9abd040a14e53d708
AvianLearning <iaradcliffe@hotmail.com>
2020-12-01 16:41:39 +0000

    Added File Controller

--- src/main/java/com/codeclan/example/filesfolders/controllers/FileController.java
+++ src/main/java/com/codeclan/example/filesfolders/controllers/FileController.java
@@ -1,4 +1,34 @@
 package com.codeclan.example.filesfolders.controllers;
 
+import com.codeclan.example.filesfolders.models.File;
+import com.codeclan.example.filesfolders.repositories.FileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class FileController {
+    @Autowired
+    FileRepository fileRepository;
+
+    @GetMapping(value = "/files")
+    public ResponseEntity<List<File>> getAllFiles() {
+        return new ResponseEntity<>(fileRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/files/{id}")
+    public ResponseEntity<Optional<File>> getFile(@PathVariable Long id) {
+        return new ResponseEntity<>(fileRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/files")
+    public ResponseEntity<File> createFile(@RequestBody File file) {
+        File newFile = fileRepository.save(file);
+        return new ResponseEntity<>(newFile, HttpStatus.CREATED);
+    }
+    
 }
