ab79c77784e8145db168596fb79ec3211b5e3559
AvianLearning <iaradcliffe@hotmail.com>
2020-12-01 16:38:33 +0000

    Created Folder Controller

--- src/main/java/com/codeclan/example/filesfolders/controllers/FolderController.java
+++ src/main/java/com/codeclan/example/filesfolders/controllers/FolderController.java
@@ -1,4 +1,33 @@
 package com.codeclan.example.filesfolders.controllers;
 
+import com.codeclan.example.filesfolders.models.Folder;
+import com.codeclan.example.filesfolders.repositories.FolderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class FolderController {
+    @Autowired
+    FolderRepository folderRepository;
+
+    @GetMapping(value = "/folders")
+    public ResponseEntity<List<Folder>> getAllFolders() {
+        return new ResponseEntity<>(folderRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/folders/{id}")
+    public ResponseEntity<Optional<Folder>> getFolder(@PathVariable Long id) {
+        return new ResponseEntity<>(folderRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/folders")
+    public ResponseEntity<Folder> createFolder(@RequestBody Folder folder) {
+        Folder newFolder = folderRepository.save(folder);
+        return new ResponseEntity<>(newFolder, HttpStatus.CREATED);
+    }
 }
